4d4d8ed045f449e9ef00fb915eb602c0d8739909
Tobias Andersen <73247328+tobias-work@users.noreply.github.com>
2021-07-29 22:00:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,48 +3,33 @@
   Author:     Tobias Andersen
   
   Playing around with the common algo challenge of finding the first non-repeating character
-  in a given string while only traversing the string up to the point a non-repeating character is detected
+  in a given string while only traversing the string up to the point a non-repeating character is detected on the second pass
 
-  Iteration count is equal to the amount of characters until detection, or string length if none found
+  Iteration count is equal to the amount of characters until detection plus string length, or twice the string length if none found
 */
 
 /* Verbose version solving the common algo of finding the first non-repeating character */
 const firstNonRepeatingCharacter = (string) => {
   
-  // Iterate each character from index 1, set insideSequence to false
-  for (index = 1, insideSequence = false, stringLength = string.length; index < stringLength; index++) {
-
-    // Skip to next iteration if previous character is a repeat
-    // Set insideSequence to true to indicate ongoing repetition
-    if (string[index] === string[index-1]) {
-      insideSequence = true
-      continue
-    }
-
-    // At this point we detect a new type of character in the string
-    // If the new character is directly after a non-sequence,
-    // the previous non-sequence character is therefore the first non-repeating character
-    if (!insideSequence) return string[index-1];
-
-    // Reset the counter since we are starting a new potential repetition
-    insideSequence = false
+  // Init lookup object
+  let object = {}
+  
+  // Pass through string once counting the character occurences
+  for (index in string) {
+    // Add 1 to the count, if character not counted before default the pre-count value as 0
+    object[string[index]] = ( object[string[index]] || 0 ) + 1
   }
 
-  // If we manage to make it to the last two characters
-  // we need to check if the last character is non-repeating and return it,
-  // Otherwise return false to indicate no non-repeating characters found
-  return string[stringLength-1] === string[stringLength-2]
-    ? false
-    : string[stringLength-1]
+  // Second pass through the string, return the function as soon as a single count has been detected.
+  // This relies on the fact that the object properties are sorted by order of insertion,
+  // so the first occurence is therefore the first non-repeating character
+  for (character in object) {
+    if (object[character] === 1) return character
+  }
+
+  // If function did not return earlier, return false to indicate no non-repeating characters
+  return false
 }
 
 /* Minified version solving the common algo of finding the first non-repeating character */
-const u=(s)=>{for(i=1,c=0,l=s.length;i<l;i++){if(s[i]===s[i-1]){c++;continue}if(!c)return s[i-1];c=false}return s[l-1]===s[l-2]?false:s[l-1]}
-
-const tests = [
-  "ssssdddKKKKjjjjwwww!wwwweskjdjfhkjwh", "aaassslllkjdoiqwjsdkjlqwe", "aaaaaaaaakkkkkkkkkkkkkkwwwwwwwwwwwjjÆÆøøåååå"
-]
-
-for (test in tests) {
-  console.log(u(tests[test]))
-}
+const u =(s)=>{let o={};for(i in s){o[s[i]]=(o[s[i]]||0)+1};for(c in o){if(o[c]===1)return c}return false}
